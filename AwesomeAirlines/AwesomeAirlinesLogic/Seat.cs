b7b5e434064bd485115ef9b8da6701c9193973fb
Nephithys <Nephithys@gmail.com>
2016-02-01 14:33:07 -0600

    created a seat object

--- AwesomeAirlines/AwesomeAirlinesLogic/Seat.cs
+++ AwesomeAirlines/AwesomeAirlinesLogic/Seat.cs
@@ -6,12 +6,49 @@ using System.Threading.Tasks;
 
 namespace AwesomeAirlinesLogic
 {
+    public enum SeatNumber
+    {
+        A = 1,
+        B = 2,
+        C = 3,
+        D = 4,
+        E = 5,
+        F = 6
+    }
+
+    public enum RowNumber
+    {
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5, 
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10
+    }
+
     public class Seat
     {
+        public SeatNumber SeatNum;
+
+        public RowNumber RowNum;
+
         public string Name;
 
-        public string SeatNumber;
-
         public double Price;
+
+        public Seat(SeatNumber seat, RowNumber row)
+        {
+            this.SeatNum = seat;
+            this.RowNum = row;
+        }
+
+        public override string ToString()
+        {
+            return RowNum + " : " + SeatNum;
+        }
     }
 }
